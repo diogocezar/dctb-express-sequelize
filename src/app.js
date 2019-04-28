32da53c653aae96931cc0f0dcd0bd949092a2691
Diogo Cezar <diogo@diogocezar.com>
2019-04-28 00:55:47 -0300

    IdHashMiddleware

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ require('dotenv').config()
 const routes = require('./routes')
 
 const { logMiddlewareStart, logMiddlewareEnd } = require('./middlewares/LogMiddleware')
+const IdHashMiddleware = require('./middlewares/IdHashMiddleware')
 const SwaggerHelper = require('./helpers/SwaggerHelper')
 
 class App {
@@ -17,6 +18,7 @@ class App {
     this.app.use(cors())
     this.app.use(express.json())
     this.app.use(logMiddlewareStart)
+    this.app.use(IdHashMiddleware)
     this.app.use(routes)
     this.app.use(logMiddlewareEnd)
     SwaggerHelper.use(this.app)
